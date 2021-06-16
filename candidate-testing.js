400363b3a24c1e47f78b363fdd2e60ae82f01985
Anita Golden-Burrell <akgolden1@users.noreply.github.com>
2021-06-16 18:10:46 +0000

    Updated code for final submission

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,23 @@ let candidateName;
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+"1) Who was the first American woman in space?",
+"2) True or false: 5 kilometer == 5000 meters?",
+"3) (5 + 3)/2 * 10 = ?",
+"4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
+"5) What is the minimum crew size for the ISS?"
+];
+
+let correctAnswers = [
+  "Sally Ride",
+  "True",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];//may not use this
+let correctAnswerCount = 0;
 
 
 function askForName() {
@@ -20,34 +34,66 @@ candidateName = input.question ("What is your name? ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
- 
-candidateAnswer = input.question (question);
+ //lines38-39 from starter code
+/*candidateAnswer = input.question (question);
 console.log (candidateAnswer);
+*/
 
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+/*for all the questions above, ask each question and store the answer in candidateAnswers. May need to be an array, or may not*/
+//next line loops through the questions
+for (let i=0; i<questions.length; i++){
+//next line asks the questions and puts the user answer into candidateAnswer, but I need it to be in an array
+candidateAnswer = input.question (questions[i]);
+
+candidateAnswers.push(candidateAnswer);
+  console.log (`Your Answer: ${candidateAnswer}`);console.log (`Correct Answer: ${correctAnswers[i]} \n` );
+//console.log(candidateAnswers);//this works to store the answers in an array!!
+//check to see if it's correct:
+
+ //no else statement needed
+ // let grade = (correctAnswerCount/10)*100;
+  //console.log (`Your current score is: ${grade}`);
+
+}
+//return grade;
+}
+
+
+// I didn't use gradeQuiz() but left it in so the code would run
+function gradeQuiz(candidateAnswers){
+let grade;
+ 
+for (let i=0; i<questions.length; i++){
+if (candidateAnswers[i] === correctAnswers[i]){
+correctAnswerCount++;
+ }
+ 
+}
+
+grade = (correctAnswerCount/questions.length)*100;
+  //try this with the return grade value. runProgram calls grade quiz
+console.log (`>>> Overall Grade:  ${grade}% (${correctAnswerCount} out of 5 respomses correct`);
+
+if (grade >= 80){
+  console.log("Status:  PASSED");
+}
+  else{
+  console.log("Status:  FAILED");
+  }
+
+
+
+
+return grade;
 
 }
 
-function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-if (candidateAnswer === correctAnswer){
-  
-  console.log ("Congradulations! You are correct!");
-}
-else {
-  console.log ("I'm sorry, please try again.");
-}
-  let grade;
-  
-
-  return grade;
-}
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log (`Welcome, ${candidateName}`);
+  console.log (`Candiate Name: ${candidateName}`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
